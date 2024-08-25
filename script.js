53b1750f4e29472e21b4620e4b6889ed42c67f4d
prasetion <prasetion@gmail.com>
2024-08-26 00:21:05 +0700

    add basic pyshics

--- script.js
+++ script.js
@@ -33,6 +33,29 @@ const environmentMapTexture = cubeTextureLoader.load([
 ]);
 
 /**
+ * Material
+ */
+
+const concreteMaterial = new CANNON.Material("concrete");
+const plasticMaterial = new CANNON.Material("plastic");
+
+/**
+ * Physics
+ */
+const world = new CANNON.World();
+world.gravity.set(0, -9.82, 0);
+
+const sphereShape = new CANNON.Sphere(0.5);
+const sphereBody = new CANNON.Body({
+  mass: 1,
+  position: new CANNON.Vec3(0, 3, 0),
+  shape: sphereShape,
+  material: plasticMaterial,
+});
+
+world.addBody(sphereBody);
+
+/**
  * Test sphere
  */
 const sphere = new THREE.Mesh(
@@ -64,6 +87,22 @@ const floor = new THREE.Mesh(
 floor.receiveShadow = true;
 floor.rotation.x = -Math.PI * 0.5;
 scene.add(floor);
+
+const floorShape = new CANNON.Plane();
+const floorBody = new CANNON.Body();
+floorBody.material = concreteMaterial;
+floorBody.mass = 0;
+floorBody.addShape(floorShape);
+floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI * 0.5);
+world.addBody(floorBody);
+
+const concretePlasticContactMaterial = new CANNON.ContactMaterial(
+  concreteMaterial,
+  plasticMaterial,
+  { friction: 0.1, restitution: 0.7 }
+);
+
+world.addContactMaterial(concretePlasticContactMaterial);
 
 /**
  * Lights
@@ -136,10 +175,17 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
  * Animate
  */
 const clock = new THREE.Clock();
+let oldElapsedTime = 0;
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - oldElapsedTime;
+  oldElapsedTime = elapsedTime;
 
+  // update physics
+  world.step(1 / 60, deltaTime, 3);
+  console.log(sphereBody.position.y);
+  sphere.position.copy(sphereBody.position);
   // Update controls
   controls.update();
 
